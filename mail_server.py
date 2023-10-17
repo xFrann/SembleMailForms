9df9a2d0b9a051fb98f73900fe399e444769bf00
Frangu Madalin <polarfrann@gmail.com>
2023-10-17 20:17:11 +0300

    Added reconnection on each request abd debug pritns

--- mail_server.py
+++ mail_server.py
@@ -4,12 +4,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import getpass
 from app_config import get_mail_host, get_mail_port, get_mail_username
-mail_server: SMTP
 email_password: str
 
 
 def connect(reconnect: bool):
-    global mail_server
     global email_password
     print("Connecting to SMTP server...")
     mail_server = smtplib.SMTP(get_mail_host(), get_mail_port())
@@ -19,21 +17,34 @@ def connect(reconnect: bool):
     if not reconnect:
         email_password = getpass.getpass(prompt="Please insert mail account password ({}): ".format(get_mail_username()))
     mail_server.login(get_mail_username(), email_password)
+    return mail_server
 
 
 def send_mail(recipient, subject, content):
-    from_mail = get_mail_username()
 
+    success = False
+    mail_server = connect(False) if email_password is None else connect(True)
+
+    from_mail = get_mail_username()
     msg = MIMEMultipart()
     msg['From'] = from_mail
     msg['To'] = recipient
     msg['Subject'] = subject
     message = content
     msg.attach(MIMEText(message))
+
     try:
+        print(f"Sending mail: [from: {from_mail}, recipient: {recipient}]")
         mail_server.sendmail(from_mail, recipient, msg.as_string())
-    except smtplib.SMTPSenderRefused as e:
-        print("SMTP Connection has timed out... reconnecting")
-        connect(True)
-    print(f"Email was sent to {recipient}")
+        success = True
+    except smtplib.SMTPException as e:
+        print(f"Exception while trying to send email {e.strerror}")
+        return success
+
+    if success:
+        print(f"Email was sent to {recipient}")
+    else:
+        print(f"FAILURE TO SEND EMAIL TO {recipient}")
+
     mail_server.quit()
+    return success
